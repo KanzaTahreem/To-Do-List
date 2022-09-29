8b22a2f85f3a0e27ef3cc8d2db669e9410dfe435
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-09-30 01:44:40 +0500

    Add function for adding todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 /* eslint-disable */
-import _ from 'lodash';
+import _, { add } from 'lodash';
 /* eslint-enable */
 import './style.css';
+import ToDoList from './todolist.js';
+import addToDo from './ui_handlers.js';
+
+const submitBtn = document.querySelector('.submit-btn');
+const addToDoInput = document.querySelector('.add-todo');
+
+const toDoList = new ToDoList();
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const inputValue = addToDoInput.value;
+  const pushedTask = toDoList.addNewTask(inputValue);
+  addToDoInput.value = '';
+  addToDo(pushedTask);
+});
--- src/todolist.js
+++ src/todolist.js
@@ -1,6 +1,14 @@
+import Task from './task.js';
+
 class ToDoList {
   constructor() {
     this.list = [];
+  }
+
+  addNewTask = (toDoInput) => {
+    const newTask = new Task(this.list.length + 1, toDoInput);
+    this.list.push(newTask);
+    return newTask;
   }
 }
 
