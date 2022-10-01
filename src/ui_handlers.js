f638ece5d587a36a39fe5d5671feec8d0e12a89b
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-01 10:34:09 +0500

    Add function to display and close menu

--- src/ui_handlers.js
+++ src/ui_handlers.js
@@ -6,14 +6,47 @@ const parser = new DOMParser();
 const addToDo = (todo, todoList) => {
   const string = `
     <li class="border-bottom">
-      <input type="checkbox" ${todo.isCompleted ? 'checked' : ''} id=${todo.index}>
-      ${todo.description}
+      <div class="list-item">
+        <input type="checkbox" ${todo.isCompleted ? 'checked' : ''} id=${todo.index}>
+        ${todo.description}
+      </div>
+      <i class="fa-regular fa-ellipsis-vertical menu"></i>
+      <ul class="task-menu hidden">
+          <li>
+            <i class="fa-solid fa-pen edit"></i>
+          </li>
+          <li>
+            <i class="fa-solid fa-trash-can delete"></i>
+          </li>
+          <li>
+            <i class="fa-solid fa-xmark close"></i>
+          </li>
+        </ul>
     </li>
   `;
 
   const todoElement = parser.parseFromString(string, 'text/html').body.firstChild;
-  const todoCompleted = todoElement.querySelector('input[type="checkbox"]');
 
+  /* function to display menu */
+
+  const menuBtn = todoElement.querySelector('.menu');
+  const taskMenu = todoElement.querySelector('.task-menu');
+  menuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    taskMenu.classList.remove('hidden');
+  });
+
+  /* function to close menu */
+
+  const closeBtn = todoElement.querySelector('.close');
+  closeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    taskMenu.classList.add('hidden');
+  });
+
+  /* function to display completed task */
+
+  const todoCompleted = todoElement.querySelector('input[type="checkbox"]');
   if (todo.isCompleted) {
     todoElement.style.textDecoration = 'line-through';
     todoElement.style.color = '#545862a3';
