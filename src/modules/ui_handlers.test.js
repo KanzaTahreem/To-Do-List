193b9a7f1dc72bdea4ffed6e99b2cb12bd4522ba
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-06 17:31:50 -0300

    Add part of DOM test for remove function

--- src/modules/ui_handlers.test.js
+++ src/modules/ui_handlers.test.js
@@ -25,13 +25,20 @@ describe('DOM', () => {
     <button type="button" class="clear-btn">Clear all completed</button>
 </section>`;
 
+  const toDoList = new ToDoList();
+  let toDoTask;
   test('Add task to UI', () => {
-    const toDoList = new ToDoList();
     UIHandler.toDoLi = document.querySelector('.list');
-    const toDoTask = toDoList.addNewTask(null, 'First Task', false);
+    toDoTask = toDoList.addNewTask(null, 'First Task', false);
     UIHandler.addToDo(toDoTask, toDoList, UIHandler.toDoLi);
     const listItems = document.querySelectorAll('.list > li');
     expect(listItems.length).toBe(1);
     expect(toDoList.list.length).toBe(1);
+  });
+  test('Remove task from UI', () => {
+    const listItems = document.querySelectorAll('.list > li');
+    toDoList.removeTask(toDoTask);
+    expect(listItems.length).toBe(0);
+    expect(toDoList.list.length).toBe(0);
   });
 });
